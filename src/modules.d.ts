7b4e8d917fb419cb75a3b5124a8ab79483bd73e7
Jackykit123 <jackykit19970402@gmail.com>
2020-09-23 06:11:27 +0800

    install types definite for textversion js package

--- src/modules.d.ts
+++
@@ -1,5 +0,0 @@
-declare module 'textversionjs' {
-    export default function textVersion(htmlString: string, config?: {
-        linkProcess(href: string, linkText: string): string
-    }): string
-}
