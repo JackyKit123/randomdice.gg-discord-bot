e19efcd3c162df01fb92820d54d212778124c085
Jackykit123 <jackykit19970402@gmail.com>
2020-09-23 16:18:57 +0800

    update if-condition

--- src/ping.ts
+++ src/ping.ts
@@ -1,18 +1,18 @@
 import * as Discord from 'discord.js';
 
 export default async function ping(message: Discord.Message): Promise<void> {
-    const { createdTimestamp, channel, guild, member } = message;
+    const { createdTimestamp, channel, member } = message;
 
     const latency = new Date().valueOf() - createdTimestamp;
 
     if (
-        guild &&
         channel.id !== process.env.DEV_SERVER_ID &&
-        member?.hasPermission('ADMINISTRATOR')
+        !member?.hasPermission('ADMINISTRATOR')
     ) {
         await channel.send(
             '`ping` command is only intended for testing purpose. Be only use it in DM channel or as `ADMINISTRATOR`.'
         );
+        return;
     }
     await message.channel.send(
         new Discord.MessageEmbed()
