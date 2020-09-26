fbfe912150f1de644f28e7a2c9d3d9585da48483
Jackykit123 <jackykit19970402@gmail.com>
2020-09-27 00:18:53 +0800

    make the help command as array

--- src/commands/help.ts
+++ src/commands/help.ts
@@ -16,25 +16,28 @@ export default async function help(message: Discord.Message): Promise<void> {
         )
         .addFields([
             {
-                name:
-                    'Configuration Commands (require MANAGE_CHANNEL permission)',
-                value:
-                    '`.gg register <guide|news|list> <channel-mention>` - register channel to sync data from the website, `list` will return a list of registered channels.\n' +
+                name: 'Sync Data (require MANAGE_CHANNEL permission)',
+                value: [
+                    '`.gg register <guide|news|list> <channel-mention>` - register channel to sync data from the website, `list` will return a list of registered channels.',
                     '`.gg postnow <guide|news>` - force syncing data from the website into your registered channels',
+                ].join('\n'),
             },
             {
                 name: 'Information',
-                value:
-                    '`.gg dice <Dice Name> [--class=?] [--level=?]` - show information about the dice, alias for arguments `-c` `-l`\n' +
+                value: [
+                    '`.gg dice <Dice Name> [--class=?] [--level=?]` - show information about the dice, alias for arguments `-c` `-l`',
                     '`.gg deck <PvP|Co-op|Crew>` - show the deck list',
+                    '`.gg guide <Guide Name>` - show the detail guide for a certain guide',
+                ].join('\n'),
             },
             {
                 name: 'Other Commands',
-                value:
-                    '`.gg ping` - ping the bot (only available in DM or as `ADMINISTRATOR`)\n' +
-                    '`.gg website [/path]` - send link to website, with optional path\n' +
-                    '`.gg app` - send link to Google Play App\n' +
+                value: [
+                    '`.gg ping` - ping the bot (only available in DM or as `ADMINISTRATOR`)',
+                    '`.gg website [/path]` - send link to website, with optional path',
+                    '`.gg app` - send link to Google Play App',
                     '`.gg support` - send information about ways to support randomdice.gg',
+                ].join('\n'),
             },
         ]);
 
