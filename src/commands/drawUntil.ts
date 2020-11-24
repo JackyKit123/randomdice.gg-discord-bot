6753c516c18b1b6986452106b6d81dce8f229580
Jackykit123 <jackykit19970402@gmail.com>
2020-11-24 20:08:28 +0800

    added different mode from card box draws

--- src/commands/drawUntil.ts
+++ src/commands/drawUntil.ts
@@ -28,74 +28,136 @@ export default async function drawUntil(
         return;
     }
 
+    const sentMessage = await channel.send('Running Simulation...');
     const legendaryPoolSize = ((await cache(
         database,
         'dice'
     )) as Dice[]).filter(d => d.rarity === 'Legendary').length;
     const simulationRuns = 100;
 
-    const [drawsForAll, drawsForFirst] = new Array(simulationRuns + 1)
-        .fill([0, 0])
-        .reduce(([accAll, accFirst]) => {
-            const target = {
-                7: 1,
-                8: 1 + 2,
-                9: 1 + 2 + 4,
-                10: 1 + 2 + 4 + 10,
-                11: 1 + 2 + 4 + 10 + 20,
-                12: 1 + 2 + 4 + 10 + 20 + 50,
-                13: 1 + 2 + 4 + 10 + 20 + 50 + 100,
-                14: 1 + 2 + 4 + 10 + 20 + 50 + 100 + 150,
-                15: 1 + 2 + 4 + 10 + 20 + 50 + 100 + 150 + 200,
-            } as { [legendaryClass: number]: number };
-            const legendaryOwned = new Array(legendaryPoolSize).fill(0);
-            let numberOfDraws = 0;
-            let numberOfDrawsForFirstLegendary = 0;
-            while (
-                legendaryOwned.some(amount => amount < target[targetClass])
-            ) {
-                const drawRandom = Math.floor(
-                    Math.random() * legendaryPoolSize
-                );
-                legendaryOwned[drawRandom] += 1;
-                numberOfDraws += 1;
-                if (
-                    legendaryOwned[drawRandom] >= target[targetClass] &&
-                    numberOfDrawsForFirstLegendary === 0
-                ) {
-                    numberOfDrawsForFirstLegendary = numberOfDraws;
-                }
-            }
-            return [
-                accAll + numberOfDraws,
-                accFirst + numberOfDrawsForFirstLegendary,
-            ];
-        });
-    await channel.send(
-        new Discord.MessageEmbed()
-            .setTitle(`Legendary Draw Simulation`)
-            .setDescription(
-                `For a pool of ${legendaryPoolSize} Legendary. This is the result for the mean number of draws to achieve legendary class ${targetClass} after running ${simulationRuns} simulations.\n*Note: it runs a simulation instead of actual math equation, the result may differ slightly each time*`
-            )
-            .setAuthor(
-                'Random Dice Community Website',
-                'https://randomdice.gg/android-chrome-512x512.png',
-                'https://randomdice.gg/'
-            )
-            .setColor('#6ba4a5')
-            .setFooter(
-                'randomdice.gg Draws Simulation',
-                'https://randomdice.gg/android-chrome-512x512.png'
-            )
-            .addField(
-                `For first class ${targetClass}:`,
-                drawsForFirst / simulationRuns,
-                true
-            )
-            .addField(
-                `For all class ${targetClass}:`,
-                drawsForAll / simulationRuns,
-                true
-            )
+    const [
+        LegendBoxAll,
+        LegendBoxFirst,
+        CardBoxAll,
+        CardBoxFirst,
+        KingsBirthAll,
+        KingsBirthFirst,
+    ] = (
+        await Promise.all(
+            new Array(simulationRuns).fill([0, 0]).map(async () => {
+                const target = {
+                    7: 1,
+                    8: 1 + 2,
+                    9: 1 + 2 + 4,
+                    10: 1 + 2 + 4 + 10,
+                    11: 1 + 2 + 4 + 10 + 20,
+                    12: 1 + 2 + 4 + 10 + 20 + 50,
+                    13: 1 + 2 + 4 + 10 + 20 + 50 + 100,
+                    14: 1 + 2 + 4 + 10 + 20 + 50 + 100 + 150,
+                    15: 1 + 2 + 4 + 10 + 20 + 50 + 100 + 150 + 200,
+                } as { [legendaryClass: number]: number };
+                const draw = async (
+                    mode: 'normal' | 'card box' | 'kings birth'
+                ): Promise<[number, number]> => {
+                    return new Promise(resolve => {
+                        const legendaryOwned = new Array(
+                            mode === 'kings birth'
+                                ? legendaryPoolSize / 2
+                                : legendaryPoolSize
+                        ).fill(0);
+                        let numberOfDraws = 0;
+                        let numberOfDrawsForFirstLegendary = 0;
+                        while (
+                            legendaryOwned.some(
+                                amount => amount < target[targetClass]
+                            )
+                        ) {
+                            numberOfDraws += 1;
+                            if (mode !== 'card box' || Math.random() < 0.01) {
+                                const drawRandom = Math.floor(
+                                    Math.random() *
+                                        (mode === 'kings birth'
+                                            ? legendaryPoolSize / 2
+                                            : legendaryPoolSize)
+                                );
+                                legendaryOwned[drawRandom] += 1;
+                                if (
+                                    legendaryOwned[drawRandom] >=
+                                        target[targetClass] &&
+                                    numberOfDrawsForFirstLegendary === 0
+                                ) {
+                                    numberOfDrawsForFirstLegendary = numberOfDraws;
+                                }
+                            }
+                        }
+                        resolve([
+                            numberOfDraws,
+                            numberOfDrawsForFirstLegendary,
+                        ]);
+                    });
+                };
+                return Promise.all([
+                    ...(await draw('normal')),
+                    ...(await draw('card box')),
+                    ...(await draw('kings birth')),
+                ]);
+            })
+        )
+    ).reduce(
+        (accumulator, currentValue) =>
+            accumulator.map((a, i) => a + currentValue[i]) as [
+                number,
+                number,
+                number,
+                number,
+                number,
+                number
+            ]
     );
+
+    const messageEmbed = new Discord.MessageEmbed()
+        .setTitle(`Legendary Draw Simulation`)
+        .setDescription(
+            `For a pool of ${legendaryPoolSize} Legendary. This is the result for the mean number of draws to achieve legendary class ${targetClass} after running ${simulationRuns} simulations.\n*Note: it runs a simulation instead of actual math equation, the result may differ slightly each time*`
+        )
+        .setAuthor(
+            'Random Dice Community Website',
+            'https://randomdice.gg/android-chrome-512x512.png',
+            'https://randomdice.gg/'
+        )
+        .setColor('#6ba4a5')
+        .setFooter(
+            'randomdice.gg Draws Simulation',
+            'https://randomdice.gg/android-chrome-512x512.png'
+        )
+        .addField(
+            `For first class ${targetClass} (from Legend Box / King's Legacy):`,
+            LegendBoxFirst / simulationRuns
+        )
+        .addField(
+            `For all class ${targetClass} (from Legend Box / King's Legacy):`,
+            LegendBoxAll / simulationRuns
+        )
+        .addField(
+            `For first class ${targetClass} (from opening Card Box)`,
+            CardBoxFirst / simulationRuns
+        )
+        .addField(
+            `For all class ${targetClass} (from opening Card Box)`,
+            CardBoxAll / simulationRuns
+        )
+        .addField(
+            `For first class ${targetClass} (from King's Birth / King's Death)`,
+            KingsBirthFirst / simulationRuns
+        )
+        .addField(
+            `For all class ${targetClass} (from King's Birth / King's Death)`,
+            (KingsBirthAll * 2) / simulationRuns
+        );
+
+    if (sentMessage.editable) {
+        await sentMessage.edit('', messageEmbed);
+    } else {
+        await channel.send(messageEmbed);
+    }
 }
