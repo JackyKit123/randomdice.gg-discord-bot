3d8a0922b60f3ecee3ddc47bc2ecc1fcb8f4d8bd
Jackykit123 <jackykit19970402@gmail.com>
2021-02-04 22:14:53 +0800

    report fn shoulb be in for community server only

--- src/bot.ts
+++ src/bot.ts
@@ -101,12 +101,12 @@ client.on('message', async function messageHandler(message) {
             return;
         }
 
-        await report(message);
         if (
             !author.bot &&
             process.env.COMMUNITY_SERVER_ID === guild?.id &&
             process.env.NODE_ENV === 'production'
         ) {
+            await report(message);
             await lock(message);
             await lfg(message);
             await eventPing(message);
