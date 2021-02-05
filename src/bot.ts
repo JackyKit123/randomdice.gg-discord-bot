f3f435bf8f88d55a16bd797c465d4158d802124b
Jackykit123 <jackykit19970402@gmail.com>
2021-02-05 20:37:55 +0800

    added missing import

--- src/bot.ts
+++ src/bot.ts
@@ -26,7 +26,7 @@ import version from './dev-commands/version';
 import cache, { fetchAll } from './helper/cache';
 import validateCrewAds from './community discord/checkCrewAds';
 import infoVC from './community discord/infoVC';
-import apply, { closeApplication } from './community discord/apply';
+import apply, { closeApplication, fetchApps } from './community discord/apply';
 import eventPing from './community discord/eventping';
 import lock from './community discord/lock';
 import report from './community discord/report';
