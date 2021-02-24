2c64d6b9e612059f77dfcca10b8239319dce8e17
Jackykit123 <jackykit19970402@gmail.com>
2021-02-24 16:49:21 +0800

    added vote reward

---
+++ src/community discord/currency/voteReward.ts
@@ -0,0 +1,33 @@
+import * as Discord from 'discord.js';
+import * as firebase from 'firebase-admin';
+import getBalance from './balance';
+
+export default async function voteReward(
+    message: Discord.Message
+): Promise<void> {
+    const { author, channel, embeds, guild } = message;
+    const database = firebase.app().database();
+
+    if (
+        author.id !== '479688142908162059' ||
+        channel.id !== '804711958116565003' ||
+        !guild
+    )
+        return;
+
+    const [embed] = embeds;
+    if (!embed) return;
+
+    const uid = embed.description.match(/\(id:(\d{18})\)/)?.[1];
+    if (!uid) return;
+
+    const member = guild.member(uid);
+    const balance = (await getBalance(message, 'silence', member)) || 10000;
+
+    await database
+        .ref(`discord_bot/community/currency/${uid}/balance`)
+        .set(balance + 1000);
+    await channel.send(
+        `Added <:Dice_TierX_Coin:813149167585067008> 1000 to ${member.user.username}'s balance. Thanks for voting us!`
+    );
+}
