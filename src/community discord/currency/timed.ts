7bffa1ae7ba13b24023cc87dc57c454a1a0016b8
Jackykit123 <jackykit19970402@gmail.com>
2021-02-25 00:07:29 +0800

    added daily streak interval reward command are now based on UTC time

--- src/community discord/currency/timed.ts
+++ src/community discord/currency/timed.ts
@@ -22,16 +22,29 @@ export default async function timed(message: Discord.Message): Promise<void> {
     if (balance === false || !member) return;
     const memberProfile = cache['discord_bot/community/currency'][member.id];
 
-    async function cooldown(timestamp = 0, cd: number): Promise<boolean> {
-        const now = Date.now().valueOf();
-        if (now - timestamp < cd) {
+    const now = new Date();
+    const msNow = now.getMilliseconds();
+    const secondsNow = now.getSeconds();
+    const minutesNow = now.getMinutes();
+    const hoursNow = now.getUTCHours();
+    const dayNow = now.getUTCDay();
+    const dateNow = now.getUTCDate();
+    const monthNow = now.getMonth();
+    const yearNow = now.getFullYear();
+
+    async function cooldown(
+        timestamp = 0,
+        excess: number,
+        cd: number
+    ): Promise<boolean> {
+        if (now.valueOf() - excess < timestamp) {
             await channel.send(
                 new Discord.MessageEmbed()
                     .setTitle('Slow Down!')
                     .setColor('#6ba4a5')
                     .setDescription(
                         `Your command is still on \`${parseMsIntoReadableText(
-                            cd + timestamp - now
+                            cd - excess
                         )}\` cooldown.`
                     )
             );
@@ -47,13 +60,32 @@ export default async function timed(message: Discord.Message): Promise<void> {
         if (rolesCache.has('804513079319592980')) multiplier = 1;
         if (rolesCache.has('804513117228367882')) multiplier = 2;
         if (rolesCache.has('805727466219372546')) multiplier = 5;
+        let streak = memberProfile.dailyStreak || 1;
+        if (mode === 'daily')
+            streak =
+                (now.valueOf() - (memberProfile.daily || 0)) /
+                    (1000 * 60 * 60 * 24) <
+                2
+                    ? streak + 1
+                    : 1;
+
         await database
             .ref(
                 `discord_bot/community/currency/${
                     (member as Discord.GuildMember).id
                 }/balance`
             )
-            .set(amount * (1 + multiplier) + (balance as number));
+            .set(
+                amount * (1 + multiplier + +0.1 * (streak - 1)) +
+                    (balance as number)
+            );
+        await database
+            .ref(
+                `discord_bot/community/currency/${
+                    (member as Discord.GuildMember).id
+                }/dailyStreak`
+            )
+            .set(streak);
         await database
             .ref(
                 `discord_bot/community/currency/${
@@ -61,38 +93,38 @@ export default async function timed(message: Discord.Message): Promise<void> {
                 }/${mode}`
             )
             .set(Date.now().valueOf());
+        const embed = new Discord.MessageEmbed()
+            .setTitle(`You claimed your ${mode} coins!`)
+            .setColor('#ffff00')
+            .setDescription(
+                `Added <:Dice_TierX_Coin:813149167585067008> ${numberFormat.format(
+                    amount * (1 + multiplier + +0.1 * (streak - 1))
+                )} to your balance!`
+            )
+            .setFooter(
+                multiplier > 0
+                    ? `+extra ${multiplier * 100}% for being $${
+                          multiplier * 10
+                      } Patreon donator`
+                    : ''
+            );
         await channel.send(
-            new Discord.MessageEmbed()
-                .setTitle(`You claimed your ${mode} coins!`)
-                .setColor('#ffff00')
-                .setDescription(
-                    `Added <:Dice_TierX_Coin:813149167585067008> ${numberFormat.format(
-                        amount * (1 + multiplier)
-                    )} to your balance!`
-                )
-                .setFooter(
-                    multiplier > 0
-                        ? `+extra ${multiplier * 100}% for being $${
-                              multiplier * 10
-                          } Patreon donator`
-                        : ''
-                )
+            streak > 1
+                ? embed.addField(
+                      'Daily Steak',
+                      `**${streak} streaks *(+${(streak - 1) * 10}% reward)***`
+                  )
+                : embed
         );
     }
-    const now = new Date();
-    const msNow = now.getUTCMilliseconds();
-    const secondsNow = now.getUTCSeconds();
-    const minutesNow = now.getUTCMinutes();
-    const hoursNow = now.getUTCHours();
-    const dayNow = now.getUTCDay();
-    const dateNow = now.getUTCDate();
 
     switch (mode) {
         case 'hourly':
             if (
                 await cooldown(
                     memberProfile.hourly,
-                    1000 * 60 * 60 - (msNow + secondsNow + minutesNow)
+                    msNow + secondsNow * 1000 + minutesNow * 1000 * 60,
+                    1000 * 60 * 60
                 )
             )
                 return;
@@ -102,8 +134,11 @@ export default async function timed(message: Discord.Message): Promise<void> {
             if (
                 await cooldown(
                     memberProfile.daily,
-                    1000 * 60 * 60 * 24 -
-                        (msNow + secondsNow + minutesNow + hoursNow)
+                    msNow +
+                        secondsNow * 1000 +
+                        minutesNow * 1000 * 60 +
+                        hoursNow * 1000 * 60 * 60,
+                    1000 * 60 * 60 * 24
                 )
             )
                 return;
@@ -113,8 +148,12 @@ export default async function timed(message: Discord.Message): Promise<void> {
             if (
                 await cooldown(
                     memberProfile.weekly,
-                    1000 * 60 * 60 * 24 * 7 -
-                        (msNow + secondsNow + minutesNow + hoursNow + dayNow)
+                    msNow +
+                        secondsNow * 1000 +
+                        minutesNow * 1000 * 60 +
+                        hoursNow * 1000 * 60 * 60 +
+                        dayNow * 1000 * 60 * 60 * 24,
+                    1000 * 60 * 60 * 24 * 7
                 )
             )
                 return;
@@ -124,8 +163,16 @@ export default async function timed(message: Discord.Message): Promise<void> {
             if (
                 await cooldown(
                     memberProfile.monthly,
-                    1000 * 60 * 60 * 24 * 30 -
-                        (msNow + secondsNow + minutesNow + hoursNow + dateNow)
+                    msNow +
+                        secondsNow * 1000 +
+                        minutesNow * 1000 * 60 +
+                        hoursNow * 1000 * 60 * 60 +
+                        dateNow * 1000 * 60 * 60 * 24,
+                    1000 *
+                        60 *
+                        60 *
+                        24 *
+                        new Date(yearNow, monthNow + 1, 0).getDate()
                 )
             )
                 return;
