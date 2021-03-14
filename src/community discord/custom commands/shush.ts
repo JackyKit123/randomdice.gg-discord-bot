bd934f4c29003e23def0fed4ca36dade92e476fa
Jackykit123 <jackykit19970402@gmail.com>
2021-03-13 22:02:13 -0800

    added custom command from patreon donor mudkipz

---
+++ src/community discord/custom commands/shush.ts
@@ -0,0 +1,109 @@
+import * as Discord from 'discord.js';
+import cooldown from '../../helper/cooldown';
+
+let shushMember: string | undefined;
+export default async function shush(message: Discord.Message): Promise<void> {
+    const { content, guild, channel, author } = message;
+
+    if (!guild || author.id !== '285696350702796801') {
+        return;
+    }
+
+    if (
+        await cooldown(message, '!shush', {
+            default: 5 * 60 * 1000,
+            donator: 5 * 60 * 1000,
+        })
+    ) {
+        return;
+    }
+
+    const memberArg = content.split(' ')[1];
+    let target: Discord.GuildMember | undefined;
+    if (memberArg) {
+        const uid =
+            memberArg.match(/^<@!?(\d{18})>$/)?.[1] ||
+            memberArg.match(/^(\d{18})$/)?.[1];
+        target = uid
+            ? await guild.members
+                  .fetch(uid)
+                  .then(u => u)
+                  .catch(err => {
+                      if (err.message === 'Unknown User') {
+                          return undefined;
+                      }
+                      throw err;
+                  })
+            : guild.members.cache.find(
+                  m =>
+                      typeof memberArg === 'string' &&
+                      memberArg !== '' &&
+                      (m.user.username.toLowerCase() ===
+                          memberArg.toLowerCase() ||
+                          `${m.user.username}#${m.user.discriminator}`.toLowerCase() ===
+                              memberArg.toLowerCase() ||
+                          (m.nickname !== null &&
+                              content
+                                  .split(' ')
+                                  .slice(1)
+                                  .join(' ')
+                                  .toLowerCase()
+                                  .startsWith(m.nickname.toLowerCase())))
+              );
+    }
+    if (!target) {
+        await channel.send(
+            `Usage of the command: \`\`\`!shush <@mention | user id | username | nickname | #username#discriminator>\`\`\``
+        );
+        return;
+    }
+    if (target.user.bot) {
+        await channel.send('You cannot trap a bot');
+        return;
+    }
+    if (target.hasPermission('ADMINISTRATOR')) {
+        await channel.send(
+            'You cannot trap a person who has `ADMINISTRATOR` permission.'
+        );
+        return;
+    }
+    shushMember = target.id;
+    await channel.send(
+        `Shush ${target}! You are trapped inside <:pokeball:820533431217815573> for 5 minutes.`
+    );
+    setTimeout(async () => {
+        shushMember = undefined;
+        await channel.send(
+            `${target} has escaped from <:pokeball:820533431217815573>.`
+        );
+    }, 1000 * 60 * 5);
+}
+
+export async function pokeballTrap(message: Discord.Message): Promise<void> {
+    const { member, deletable, channel, content } = message;
+
+    if (!member || member.id !== shushMember) {
+        return;
+    }
+
+    if (deletable) {
+        try {
+            await message.delete({ reason: 'Fun Command' });
+        } catch {
+            // do nothing
+        }
+    }
+
+    const randomString = [
+        `**${member.displayName}** is trapped in a <:pokeball:820533431217815573>: ||${content}||`,
+        `**${member.displayName}** is yelling from inside the <:pokeball:820533431217815573>: ||${content}||`,
+        `A sound from a distant <:pokeball:820533431217815573>, **${member.displayName}** says: ||${content}||`,
+        `<:pokeball:820533431217815573>**${member.displayName}**<:pokeball:820533431217815573>\n||${content}||`,
+    ];
+    await channel.send(
+        randomString[Math.floor(Math.random() * randomString.length)],
+        {
+            disableMentions: 'all',
+        }
+    );
+}
