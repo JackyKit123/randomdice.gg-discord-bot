257006cf704d4b1e910fe2f74efa475c4b900da8
Jackykit123 <jackykit19970402@gmail.com>
2021-03-20 23:54:49 -0700

    ignore bot user

--- src/community discord/oneMinute.ts
+++ src/community discord/oneMinute.ts
@@ -4,6 +4,7 @@ export default async function oneMinute(
     message: Discord.Message
 ): Promise<void> {
     const { content, author, channel } = message;
+    if (author.bot) return;
 
     if (/\b(one|1) ?min(ute)?\b/.test(content)) {
         setTimeout(
