5ab48f5ae72b1d71bdf0c0831982c1b020601688
Jackykit123 <jackykit19970402@gmail.com>
2021-03-22 19:24:18 -0700

    sending a message instead of silently ignoring custom commands

--- src/community discord/custom commands/moon.ts
+++ src/community discord/custom commands/moon.ts
@@ -17,97 +17,105 @@ export default async function custom(
 
     const memberArg = content.split(' ')?.[1];
 
-    if (author.id === '722951439567290458') {
-        if (
-            await cooldown(message, '!moon', {
-                default: 10 * 1000,
-                donator: 10 * 1000,
-            })
-        ) {
-            return;
-        }
-        const target = await fetchMention(memberArg, guild, {
-            content,
-            mentionIndex: 1,
-        });
-        if (!target) {
-            await channel.send(
-                `Usage of the command: \`\`\`!moon <@mention | user id | username | nickname | #username#discriminator>\`\`\``
-            );
-            return;
-        }
-        if (target.id === author.id) {
-            await channel.send(
-                `${author.toString()}, why are your mooning yourself? <:what:770517830852542475>`
-            );
-            return;
-        }
-        if (target.roles.cache.has('804508975503638558')) {
-            await channel.send(
-                `${target.toString()} has already been mooned, wait a bit before your moon again.`
-            );
-            return;
-        }
-        const clientRole = (guild.member(
-            (client.user as Discord.ClientUser).id
-        ) as Discord.GuildMember).roles.highest;
-        const targetRole = target.roles.highest;
-        if (clientRole.comparePositionTo(targetRole) <= 0) {
-            await channel.send(
-                `I am not high enough in the role hierarchy to \`moon\` this member.`
-            );
-            return;
-        }
-        const originalName = target.displayName;
-        const randomMoonEmoji = [
-            '🌝',
-            '🌕',
-            '🌗',
-            '🌘',
-            '🌖',
-            '🌙',
-            '🌛',
-            '🌚',
-            '🌑',
-            '🌓',
-            '🌒',
-            '🌔',
-            '☪',
-            '☾',
-            '☽',
-        ][Math.floor(Math.random() * 15)];
-        const sentMessage = await channel.send(`${target.toString()}...🌚`);
-        await wait(500);
-        await sentMessage.edit(`${target.toString()}...🌘`);
-        await wait(500);
-        await sentMessage.edit(`${target.toString()}...🌗`);
-        await wait(500);
-        await sentMessage.edit(`${target.toString()}...🌗`);
-        await wait(500);
-        await sentMessage.edit(`${target.toString()}...🌖`);
-        await wait(500);
-        await sentMessage.edit(`${target.toString()}...🌝`);
-        await wait(500);
-        await Promise.all([
-            target.setNickname(
-                originalName.length >= 30
-                    ? `${originalName.slice(0, 29)}…${randomMoonEmoji}`
-                    : `${originalName}${randomMoonEmoji}`
-            ),
-            target.roles.add('804508975503638558'),
-        ]);
-        await sentMessage.edit(
-            `${target.toString()}...You have been mooned! <a:Taxi:780350572212781086>`
+    if (author.id !== '722951439567290458') {
+        const member = guild.member('722951439567290458');
+        const memberStr = member
+            ? `**${member.user.username}#${member.user.discriminator}**`
+            : '<@722951439567290458>';
+        await channel.send(
+            `This is a private command dedicated to ${memberStr} as a perk of $50 Patreon Donator.`
         );
-        await wait(1000 * 60 * 5);
-        try {
-            if (target.roles.cache.has('804508975503638558')) {
-                await target.roles.remove('804508975503638558');
-            }
-            await target.setNickname(originalName);
-        } catch (err) {
-            // suppress error
+        return;
+    }
+    if (
+        await cooldown(message, '!moon', {
+            default: 10 * 1000,
+            donator: 10 * 1000,
+        })
+    ) {
+        return;
+    }
+    const target = await fetchMention(memberArg, guild, {
+        content,
+        mentionIndex: 1,
+    });
+    if (!target) {
+        await channel.send(
+            `Usage of the command: \`\`\`!moon <@mention | user id | username | nickname | #username#discriminator>\`\`\``
+        );
+        return;
+    }
+    if (target.id === author.id) {
+        await channel.send(
+            `${author.toString()}, why are your mooning yourself? <:what:770517830852542475>`
+        );
+        return;
+    }
+    if (target.roles.cache.has('804508975503638558')) {
+        await channel.send(
+            `${target.toString()} has already been mooned, wait a bit before your moon again.`
+        );
+        return;
+    }
+    const clientRole = (guild.member(
+        (client.user as Discord.ClientUser).id
+    ) as Discord.GuildMember).roles.highest;
+    const targetRole = target.roles.highest;
+    if (clientRole.comparePositionTo(targetRole) <= 0) {
+        await channel.send(
+            `I am not high enough in the role hierarchy to \`moon\` this member.`
+        );
+        return;
+    }
+    const originalName = target.displayName;
+    const randomMoonEmoji = [
+        '🌝',
+        '🌕',
+        '🌗',
+        '🌘',
+        '🌖',
+        '🌙',
+        '🌛',
+        '🌚',
+        '🌑',
+        '🌓',
+        '🌒',
+        '🌔',
+        '☪',
+        '☾',
+        '☽',
+    ][Math.floor(Math.random() * 15)];
+    const sentMessage = await channel.send(`${target.toString()}...🌚`);
+    await wait(500);
+    await sentMessage.edit(`${target.toString()}...🌘`);
+    await wait(500);
+    await sentMessage.edit(`${target.toString()}...🌗`);
+    await wait(500);
+    await sentMessage.edit(`${target.toString()}...🌗`);
+    await wait(500);
+    await sentMessage.edit(`${target.toString()}...🌖`);
+    await wait(500);
+    await sentMessage.edit(`${target.toString()}...🌝`);
+    await wait(500);
+    await Promise.all([
+        target.setNickname(
+            originalName.length >= 30
+                ? `${originalName.slice(0, 29)}…${randomMoonEmoji}`
+                : `${originalName}${randomMoonEmoji}`
+        ),
+        target.roles.add('804508975503638558'),
+    ]);
+    await sentMessage.edit(
+        `${target.toString()}...You have been mooned! <a:Taxi:780350572212781086>`
+    );
+    await wait(1000 * 60 * 5);
+    try {
+        if (target.roles.cache.has('804508975503638558')) {
+            await target.roles.remove('804508975503638558');
         }
+        await target.setNickname(originalName);
+    } catch (err) {
+        // suppress error
     }
 }
 
