7e28d3fa15450f9a8c7c439529dcf529bb5cd23a
Jacky <jackykit19970402@gmail.com>
2021-05-29 11:10:14 -0700

    removed unused import

--- src/community discord/cleverbot.ts
+++ src/community discord/cleverbot.ts
@@ -1,6 +1,5 @@
 import * as Discord from 'discord.js';
 import axios from 'axios';
-import cache from '../util/cache';
 import cooldown from '../util/cooldown';
 import { replaceAllMentionToText } from '../util/fetchMention';
 
