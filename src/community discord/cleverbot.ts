2e61f9c78f528d3330635b5c3ebc6ac7ae025425
Jacky <jackykit19970402@gmail.com>
2021-05-29 11:06:42 -0700

    Allow clever bot to be used by everyone

--- src/community discord/cleverbot.ts
+++ src/community discord/cleverbot.ts
@@ -12,56 +12,35 @@ export default async function cleverBot(
     const { content, channel, client, author, guild } = message;
     if (!client.user || !guild) return;
 
-    const { users } = cache;
-    const userIsDonator = Object.values(users).some(
-        user =>
-            user['linked-account'].discord === author.id &&
-            Boolean(user['patreon-tier'])
-    );
     let input = content.match(new RegExp(`<@!?${client.user.id}> ?(.*)`))?.[1];
     if (!input) return;
     input = replaceAllMentionToText(input, guild);
     if (
         await cooldown(message, 'cleverbot', {
-            default: 10 * 1000,
-            donator: 0,
+            default: 20 * 1000,
+            donator: 2 * 1000,
         })
     ) {
         return;
     }
-    if (
-        !userIsDonator &&
-        author.id !== '195174308052467712' &&
-        (channel as Discord.TextChannel).parentID !== '804227071765118976'
-    ) {
-        await channel.send(
-            new Discord.MessageEmbed()
-                .setTitle('I cannot respond here.')
-                .setColor('#ff0000')
-                .setDescription(
-                    `Responding in ${channel} is only allowed for [Patreon donators](https://www.patreon.com/RandomDiceCommunityWebsite), a $5 donation would enable you to talk with me here. However, you can still interact with me in <#804227071765118976>.`
-                )
-        );
-        return;
-    }
 
-    const cs = conversationIds.get(channel.id);
+    const cs = conversationIds.get(`${channel.id}${author.id}`);
     const res = await axios.get(
         `http://www.cleverbot.com/getreply?key=${
             process.env.CLEVER_BOT_API_KEY
         }&input=${input}${cs ? `&cs=${cs}` : ''}`
     );
-    conversationIds.set(channel.id, res.data.cs);
+    conversationIds.set(`${channel.id}${author.id}`, res.data.cs);
     await channel.send(res.data.output);
     // reset conversation id after 10 minutes
-    const timeout = conversationTimeout.get(channel.id);
+    const timeout = conversationTimeout.get(`${channel.id}${author.id}`);
     if (timeout) {
         clearTimeout(timeout);
     }
     conversationTimeout.set(
-        channel.id,
+        `${channel.id}${author.id}`,
         setTimeout(() => {
-            conversationIds.set(channel.id, null);
-        }, 10 * 60 * 1000)
+            conversationIds.set(`${channel.id}${author.id}`, null);
+        }, 30 * 1000)
     );
 }
