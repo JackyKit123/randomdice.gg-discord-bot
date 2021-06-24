7da569b2c3b0c3f2d7b041a153eb615add6dcd5c
JackyKit <jackykit19970402@gmail.com>
2021-06-24 18:57:18 +0800

    8ball auto responder should ignore rd channels

--- src/community discord/8ball.ts
+++ src/community discord/8ball.ts
@@ -3,7 +3,16 @@ import Discord from 'discord.js';
 export default async function eightBall(
     message: Discord.Message
 ): Promise<void> {
+    const { channel } = message;
     const regex = /^(does|do|did|wanna|should|is|are|am|should|may|shall|was|were|will|can|could|would|have|had|havn't|hadn't|haven't|didn't|don't|wouldn't|isn't|aren't|ain't|shouldn't|won't|can't|weren't|couldn't)( .+){1,}\?/i;
+    if (
+        [
+            '804229330443436032',
+            '804230784205979649',
+            '804224162364129320',
+        ].includes(channel.id)
+    )
+        return;
     if (message.content.match(regex)) {
         const eightballAns = [
             'It is certain',
@@ -28,6 +37,6 @@ export default async function eightBall(
             'Very doubtful',
         ];
         const randomNumber = Math.floor(Math.random() * 20);
-        await message.reply(eightballAns[randomNumber]);
+        await message.channel.send(eightballAns[randomNumber]);
     }
 }
