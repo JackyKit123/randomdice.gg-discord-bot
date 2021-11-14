bb432bba042df993f80ab8fd0c631de8337f7f3d
JackyKit <jackykit19970402@gmail.com>
2021-11-13 17:54:49 -0800

    fixing member who left cannot be rejected in ban appeal server

--- src/community discord/ban appeal/closeAppeal.ts
+++ src/community discord/ban appeal/closeAppeal.ts
@@ -30,58 +30,73 @@ export default async function closeAppeal(message: Message): Promise<void> {
         throw new Error('Missing `COMMUNITY_SERVER_ID` env in bot code.');
     }
 
-    const accept = async (target: GuildMember): Promise<void> => {
+    const accept = async (target: GuildMember | string): Promise<void> => {
+        (await client.guilds.fetch(COMMUNITY_SERVER_ID)).members.unban(
+            target,
+            'Appealed accepted in appeal server.'
+        );
+
         try {
-            (await client.guilds.fetch(COMMUNITY_SERVER_ID)).members.unban(
-                target,
-                'Appealed accepted in appeal server.'
-            );
-        } finally {
-            try {
+            if (target instanceof GuildMember)
                 await target.send(
-                    'Your appeal is accepted, you may now return to this main server. https://gg/ZrXRpZq2mq'
+                    'Your appeal is accepted, you may now return to this main server. https://discord.gg/ZrXRpZq2mq'
                 );
-            } finally {
-                await target.ban({
-                    reason: 'Appeal accepted.',
-                });
-                await channel.send(
-                    `Accepted appeal for **${target.user.username}#${target.user.discriminator}**.`
-                );
-            }
+        } finally {
+            guild.members.ban(target, {
+                reason: 'Appeal accepted.',
+            });
+            await channel.send(
+                `Accepted appeal for ${
+                    target instanceof GuildMember
+                        ? `**${target.user.username}#${target.user.discriminator}**`
+                        : `<@${target}>`
+                }.`
+            );
         }
     };
 
-    const reject = async (target: GuildMember): Promise<void> => {
+    const reject = async (target: GuildMember | string): Promise<void> => {
         try {
-            await target.send('Your appeal is rejected.');
+            if (target instanceof GuildMember)
+                await target.send('Your appeal is rejected.');
         } finally {
-            await target.ban({
+            guild.members.ban(target, {
                 reason: 'Appeal rejected.',
             });
             await channel.send(
-                `Rejected appeal for **${target.user.username}#${target.user.discriminator}**.`
+                `Rejected appeal for ${
+                    target instanceof GuildMember
+                        ? `**${target.user.username}#${target.user.discriminator}**`
+                        : `<@${target}>`
+                }.`
             );
         }
     };
 
-    const falsebanned = async (target: GuildMember): Promise<void> => {
+    const falsebanned = async (target: GuildMember | string): Promise<void> => {
+        (await client.guilds.fetch(COMMUNITY_SERVER_ID)).members.unban(
+            target,
+            'Appealed accepted in appeal server, member is not guilty.'
+        );
+
         try {
-            (await client.guilds.fetch(COMMUNITY_SERVER_ID)).members.unban(
-                target,
-                'Appealed accepted in appeal server, member is not guilty.'
-            );
-        } finally {
-            try {
+            if (target instanceof GuildMember)
                 await target.send(
-                    'Your appeal is accepted, you are found to be clean, you may now return to this main server. https://gg/ZrXRpZq2mq'
+                    'Your appeal is accepted, you are found to be clean, you may now return to this main server. https://discord.gg/ZrXRpZq2mq'
                 );
-            } finally {
-                await target.kick('Member is not guilty, appeal closed.');
-                await channel.send(
-                    `Closed appeal for **${target.user.username}#${target.user.discriminator}**. User is clean.`
-                );
-            }
+        } finally {
+            await guild.members.kick(
+                target,
+                'Member is not guilty, appeal closed.'
+            );
+
+            await channel.send(
+                `Closed appeal for ${
+                    target instanceof GuildMember
+                        ? `**${target.user.username}#${target.user.discriminator}**`
+                        : `<@${target}>`
+                }. User is clean.`
+            );
         }
     };
 
@@ -89,39 +104,54 @@ export default async function closeAppeal(message: Message): Promise<void> {
         return;
     }
 
-    const target = await fetchMentionString(arg, guild, {
-        content,
-        mentionIndex: 1,
-    });
+    let target: string | GuildMember | undefined = await fetchMentionString(
+        arg,
+        guild,
+        {
+            content,
+            mentionIndex: 1,
+        }
+    );
     if (!target) {
-        await channel.send(
-            `Usage of the command: \`\`\`${command} <@mention | user id | username>\`\`\``
-        );
-        return;
-    }
-    const executorRole = member.roles.highest;
-    const executorCanBan = member.permissions.has('BAN_MEMBERS');
-    const clientRole = (
-        guild.members.cache.get((client.user as ClientUser).id) as GuildMember
-    ).roles.highest;
-    const targetRole = target.roles.highest;
-
-    if (executorRole.comparePositionTo(targetRole) < 0) {
-        return;
+        target =
+            arg.match(/^<@!?(\d{18})>$/)?.[1] || arg.match(/^(\d{18})$/)?.[1];
+        if (!target) {
+            await channel.send(
+                `Usage of the command: \`\`\`${command} <@mention | user id | username>\`\`\``
+            );
+            return;
+        }
     }
 
-    if (executorRole.comparePositionTo(targetRole) <= 0 || !executorCanBan) {
+    if (!member.permissions.has('BAN_MEMBERS')) {
         await channel.send(
             'You do not have sufficient permission to execute this command.'
         );
         return;
     }
 
-    if (clientRole.comparePositionTo(targetRole) <= 0) {
-        await channel.send(
-            'I do not have sufficient permission to execute this command.'
-        );
-        return;
+    if (target instanceof GuildMember) {
+        const executorRole = member.roles.highest;
+        const targetRole = target.roles.highest;
+        const clientRole = (
+            guild.members.cache.get(
+                (client.user as ClientUser).id
+            ) as GuildMember
+        ).roles.highest;
+
+        if (executorRole.comparePositionTo(targetRole) < 0) {
+            await channel.send(
+                'You do not have sufficient permission to ban or unban this user.'
+            );
+            return;
+        }
+
+        if (clientRole.comparePositionTo(targetRole) <= 0) {
+            await channel.send(
+                'I do not have sufficient permission to execute this command.'
+            );
+            return;
+        }
     }
 
     const archiveCategories = guild.channels.cache.filter(
