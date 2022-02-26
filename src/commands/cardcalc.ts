c7dc278d7554d7dc6ca5089524a7c64d44e783c6
Wade <wadewilson69420@gmail.com>
2022-02-25 21:12:49 -0500

    card calc for update + fixed lfg error message

--- src/commands/cardcalc.ts
+++ src/commands/cardcalc.ts
@@ -4,7 +4,7 @@ import cooldown from '../util/cooldown';
 export default async function cardcalc(message: Discord.Message) {
     const { channel, content } = message;
     const [command, arg] = content.split(' ');
-    let waves = 0;
+    const waves = Number(arg);
     if (
         await cooldown(message, command, {
             default: 10 * 1000,
@@ -15,15 +15,12 @@ export default async function cardcalc(message: Discord.Message) {
     }
 
     if (
-        !Number.isNaN(arg) &&
-        Number.isInteger(Number(arg)) &&
-        Number(arg) > 0
+        !Number.isInteger(waves) ||
+        Number(waves) < 0
     ) {
-        waves = Number(arg);
-    } else {
         await channel.send('waves must be positive integer');
         return;
-    }
+    } 
 
     if (waves > 100000) {
         await channel.send('not feasible keep dreaming');
@@ -31,7 +28,7 @@ export default async function cardcalc(message: Discord.Message) {
     }
 
     let cards = 0;
-    if (waves <= 40) {
+    if (waves <= 45) {
         const isMiniBossWave = waves % 5 === 3 || waves % 5 === 4; // check if mini boss wave
         cards += Math.floor(waves / 5) * 8; // 8 cards per 5 wave
         cards += waves % 5; // 1 card for each wave
@@ -39,9 +36,9 @@ export default async function cardcalc(message: Discord.Message) {
             cards += 1; // 1 extra card for mini boss
         }
     } else {
-        cards = 64; // 64 cards for 40 waves
-        cards += Math.floor((waves - 40) / 2) * 8; // 8 cards per 2 waves
-        cards += (waves - 40) % 2; // 1 card for each wave
+        cards = 72; // 72 cards for 45 waves (add 8 for every new boss)
+        cards += Math.floor((waves - 45) / 2) * 8; // 8 cards per 2 waves
+        cards += (waves - 45) % 2; // 2 card for each non boss wave 
     }
 
     await channel.send(
