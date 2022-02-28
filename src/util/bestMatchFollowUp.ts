2ab8cd9226263d3467e41374e4e92b8a718c8d03
JackyKit <jackykit19970402@gmail.com>
2022-02-28 04:34:41 -0800

    adding error catcher in the follow up function

--- src/util/bestMatchFollowUp.ts
+++ src/util/bestMatchFollowUp.ts
@@ -1,3 +1,4 @@
+import logMessage from 'dev-commands/logMessage';
 import {
     CommandInteraction,
     Message,
@@ -44,50 +45,91 @@ export default async function bestMatchFollowUp(
     });
 
     let answeredYes = false;
+
     sentMessage
         .createMessageComponentCollector({
             time: 60000,
         })
         .on('collect', async collected => {
-            if (
-                collected.user.id !==
-                (
-                    (input as Message).author ||
-                    (input as CommandInteraction).user
-                ).id
-            ) {
-                collected.reply({
-                    content:
-                        'You cannot use this button because you did not initiate this command.',
-                    ephemeral: true,
-                });
-                return;
-            }
-            if (collected.customId === 'yes') {
-                answeredYes = true;
-                const newResponse = newMatch(bestMatch.target);
-                const messageOption =
-                    typeof newResponse === 'string'
-                        ? { content: newResponse, components: [] }
-                        : {
-                              ...newResponse,
-                              components: [],
-                              content: undefined,
-                          };
-                edit(
-                    input instanceof CommandInteraction ? input : sentMessage,
-                    messageOption
-                );
-            } else if (collected.customId === 'no') {
-                await sentMessage.delete();
+            try {
+                if (
+                    collected.user.id !==
+                    (
+                        (input as Message).author ||
+                        (input as CommandInteraction).user
+                    ).id
+                ) {
+                    collected.reply({
+                        content:
+                            'You cannot use this button because you did not initiate this command.',
+                        ephemeral: true,
+                    });
+                    return;
+                }
+                if (collected.customId === 'yes') {
+                    answeredYes = true;
+                    const newResponse = newMatch(bestMatch.target);
+                    const messageOption =
+                        typeof newResponse === 'string'
+                            ? { content: newResponse, components: [] }
+                            : {
+                                  ...newResponse,
+                                  components: [],
+                                  content: undefined,
+                              };
+                    await edit(
+                        input instanceof CommandInteraction
+                            ? input
+                            : sentMessage,
+                        messageOption
+                    );
+                } else if (collected.customId === 'no') {
+                    await sentMessage.delete();
+                }
+            } catch (err) {
+                try {
+                    await logMessage(
+                        input.client,
+                        `Oops, something went wrong in <#${
+                            input.channelId
+                        }> : ${
+                            (err as Error).stack ??
+                            (err as Error).message ??
+                            err
+                        }\n while collecting message component.`
+                    );
+                } catch (criticalError) {
+                    // eslint-disable-next-line no-console
+                    console.error(criticalError);
+                }
             }
         })
         .on('end', async () => {
             if (!answeredYes) {
-                await edit(
-                    input instanceof CommandInteraction ? input : sentMessage,
-                    invalidMessage
-                );
+                try {
+                    await edit(
+                        input instanceof CommandInteraction
+                            ? input
+                            : sentMessage,
+                        invalidMessage
+                    );
+                } catch (err) {
+                    try {
+                        await logMessage(
+                            input.client,
+                            `Oops, something went wrong in <#${
+                                input.channelId
+                            }> : ${
+                                (err as Error).stack ??
+                                (err as Error).message ??
+                                err
+                            }\n while collecting message component.`
+                        );
+                    } catch (criticalError) {
+                        // eslint-disable-next-line no-console
+                        console.error(criticalError);
+                    }
+                }
             }
         });
 }
