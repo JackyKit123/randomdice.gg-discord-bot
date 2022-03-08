90711ad8b3ee4ae1450cb7d47235c8e0c0c60c2c
JackyKit <jackykit19970402@gmail.com>
2022-03-08 13:13:50 -0800

    report command is now slash command only

--- src/community discord/report.ts
+++ src/community discord/report.ts
@@ -1,60 +1,55 @@
-import Discord, {
+import { moderatorRoleIds } from 'config/roleId';
+import {
     ApplicationCommandData,
     CommandInteraction,
     ContextMenuInteraction,
-    DiscordAPIError,
-    GuildMember,
-    Message,
-    TextBasedChannel,
+    MessageEmbed,
+    Permissions,
+    CategoryChannel,
 } from 'discord.js';
 import cooldown from 'util/cooldown';
 import parseMsIntoReadableText from 'util/parseMS';
-import { reply } from 'util/typesafeReply';
 
 export async function closeReport(
-    input: Message | CommandInteraction
+    interaction: CommandInteraction
 ): Promise<void> {
-    const { channel, guild } = input;
+    if (!interaction.inCachedGuild()) return;
+    const { channel, guild, member, commandName } = interaction;
 
-    const member = guild?.members.cache.get(input.member?.user.id ?? '');
     if (
-        await cooldown(input, '!closereport', {
+        (await cooldown(interaction, commandName, {
             default: 10 * 1000,
             donator: 10 * 1000,
-        })
+        })) ||
+        !channel?.isText() ||
+        channel.isThread()
     ) {
         return;
     }
-    if (!guild || !member || channel?.type !== 'GUILD_TEXT') {
-        return;
-    }
-    const content =
-        input instanceof Message
-            ? input.content.replace('!closereport', '').trim()
-            : input.options.getString('reason') ?? '';
-    const modRoleId = '804223928427216926';
-    const tModRoleId = '807219483311603722';
+
+    const reason = interaction.options.getString('reason')?.trim() ?? '';
+
     if (
         !(
-            member.roles.cache.has(modRoleId) ||
-            member.roles.cache.has(tModRoleId) ||
+            member.roles.cache.hasAny(...moderatorRoleIds) ||
             channel.permissionsFor(member)?.has('MANAGE_CHANNELS')
         )
     ) {
-        await reply(input, 'You do not have permission to close this report.');
+        await interaction.reply(
+            'You do not have permission to close this report.'
+        );
         return;
     }
     if (!channel.name.endsWith('-report-room')) {
-        await reply(input, 'You cannot use this command here.');
+        await interaction.reply('You cannot use this command here.');
         return;
     }
     if (channel.parentId === '806634775514710106') {
-        await reply(input, 'This report is already closed.');
+        await interaction.reply('This report is already closed.');
         return;
     }
-    if (content.length > 1024) {
-        await reply(
-            input,
+    if (reason.length > 1024) {
+        await interaction.reply(
             'Close report reason should be less than 1024 characters.'
         );
         return;
@@ -67,15 +62,13 @@ export async function closeReport(
     const archiveCat = guild.channels.cache.get('806634775514710106');
 
     if (archiveCat?.type !== 'GUILD_CATEGORY') {
-        await reply(
-            input,
+        await interaction.reply(
             'Unable to retrieve Archive channel category, please contact an admin to manually drag this channel into Archive category.'
         );
         return;
     }
     if (archiveCat.children.size >= 50) {
-        await reply(
-            input,
+        await interaction.reply(
             'There are too many reports in the archive category. Please delete some reports.'
         );
         return;
@@ -84,40 +77,8 @@ export async function closeReport(
         reason: 'Report Closed',
     });
     const reportLog = guild.channels.cache.get('806812461302022145');
-    const now = Date.now();
+
     if (reportLog?.isText()) {
-        let embed = new Discord.MessageEmbed()
-            .setTitle('Report Closed')
-            .setDescription(content.replace('!closereport', ''))
-            .addField(
-                'Report Closed By',
-                `**Name:** ${member.user.tag} ${member.toString()}` +
-                    '\n' +
-                    `**ID:** ${member.user.id}`
-            )
-            .setFooter({ text: 'Report Closed at' })
-            .setTimestamp();
-        if (reportMember) {
-            embed = embed.setAuthor({
-                name: reportMember.user.tag,
-                iconURL: reportMember.displayAvatarURL({
-                    dynamic: true,
-                }),
-            });
-            embed.addField(
-                'Report Member',
-                `**Name:** ${
-                    reportMember.user.tag
-                } ${reportMember.toString()}` +
-                    '\n' +
-                    `**Reported At**: ${parseMsIntoReadableText(
-                        now - channel.createdTimestamp,
-                        true
-                    )} ago` +
-                    '\n' +
-                    `**ID:** ${member.user.id}`
-            );
-        }
         const pinnedMessages = await channel.messages.fetchPinned();
         const reportLogMessages = await reportLog.messages.fetch();
         const initialLogMessage = reportLogMessages.find(msg =>
@@ -130,62 +91,57 @@ export async function closeReport(
                 )
             )
         );
+
+        const editCloseReportLogEmbed = (embed: MessageEmbed) => {
+            const edited = embed
+                .setTitle('Report Closed')
+                .setColor('#000000')
+                .addField(
+                    'Report Closed by',
+                    `**Name:** ${member.user.username}#${
+                        member.user.discriminator
+                    } ${member.toString()}` +
+                        '\n' +
+                        `**Closed at**: ${interaction.createdAt.toUTCString()}` +
+                        '\n' +
+                        `**ID:** ${member.user.id}`
+                );
+            if (reason) {
+                return edited.addField('With reason', reason);
+            }
+            return edited;
+        };
+
         if (initialLogMessage) {
             await initialLogMessage.edit({
-                embeds: initialLogMessage.embeds.map(emb => {
-                    const edited = emb
-                        .setTitle('Report Closed')
-                        .setColor('#000000')
-                        .addField(
-                            'Report Closed by',
-                            `**Name:** ${member.user.username}#${
-                                member.user.discriminator
-                            } ${member.toString()}` +
-                                '\n' +
-                                `**Closed at**: ${input.createdAt.toUTCString()}` +
-                                '\n' +
-                                `**ID:** ${member.user.id}`
-                        );
-                    if (content.replace('!closereport', '').trim()) {
-                        return edited.addField(
-                            'With reason',
-                            content.replace('!closereport', '').trim()
-                        );
-                    }
-                    return edited;
-                }),
+                embeds: initialLogMessage.embeds.map(editCloseReportLogEmbed),
             });
         } else {
-            await reportLog.send({ embeds: [embed] });
+            await reportLog.send({
+                embeds: [editCloseReportLogEmbed(new MessageEmbed())],
+            });
         }
     }
     const { everyone } = guild.roles;
-    const modRole = guild.roles.cache.get(modRoleId);
-    const tModRole = guild.roles.cache.get(tModRoleId);
     await channel.permissionOverwrites.set(
         [
             {
                 id: everyone,
-                deny: ['VIEW_CHANNEL', 'SEND_MESSAGES'],
+                deny: new Permissions(['VIEW_CHANNEL', 'SEND_MESSAGES']),
             },
-            {
-                id: modRole ?? '',
-                allow: ['VIEW_CHANNEL'],
-            },
-            {
-                id: tModRole ?? '',
-                allow: ['VIEW_CHANNEL'],
-            },
+            ...moderatorRoleIds.map(id => ({
+                id,
+                allow: new Permissions(['VIEW_CHANNEL']),
+            })),
         ],
         'Archive Channel.'
     );
-    await reply(
-        input,
+    await interaction.reply(
         `Report closed, removed access for everyone, archiving this channel.`
     );
     await reportMember?.send({
         embeds: [
-            new Discord.MessageEmbed()
+            new MessageEmbed()
                 .setTitle('Report Closed')
                 .setColor('#6ba4a5')
                 .setAuthor({
@@ -194,7 +150,7 @@ export async function closeReport(
                         dynamic: true,
                     }),
                 })
-                .setDescription(content.replace('!closereport', ''))
+                .setDescription(reason)
                 .setFooter({ text: 'Report closed at' })
                 .setTimestamp(),
         ],
@@ -202,24 +158,21 @@ export async function closeReport(
 }
 
 export async function report(
-    input: Message | CommandInteraction | ContextMenuInteraction
+    interaction: CommandInteraction | ContextMenuInteraction
 ): Promise<void> {
-    const { guild, channel, createdTimestamp } = input;
+    if (!interaction.inCachedGuild()) return;
 
-    const member = guild?.members.cache.get(input.member?.user.id ?? '');
+    const { guild, member, channel, createdTimestamp } = interaction;
 
     const content =
-        // eslint-disable-next-line no-nested-ternary
-        (input instanceof Message
-            ? input.content.replace('!report', '').trim()
-            : input instanceof CommandInteraction
-            ? input.options.getString('message')
-            : input.options.getMessage('message')?.content) ?? 'not specific';
-    if (!guild || !member) {
-        return;
-    }
+        interaction instanceof CommandInteraction
+            ? interaction.options.getString('message') ?? 'not specific'
+            : `Message Reported:\n${
+                  interaction.options.getMessage('message', true)?.content
+              }`;
+
     if (
-        await cooldown(input, '!report', {
+        await cooldown(interaction, '!report', {
             default: 10 * 1000,
             donator: 10 * 1000,
         })
@@ -227,29 +180,16 @@ export async function report(
         return;
     }
 
-    if (input instanceof Message) {
-        try {
-            await input.delete();
-        } catch (err) {
-            if ((err as DiscordAPIError).message !== 'Unknown Message')
-                throw err;
-        }
-    }
     const now = Date.now();
-    const messageLink =
-        input instanceof ContextMenuInteraction
-            ? input.options.getMessage('message')?.id
-            : input.channel?.lastMessageId;
-    const memberMentions =
-        // eslint-disable-next-line no-nested-ternary
-        input instanceof ContextMenuInteraction
-            ? [input.options.getMessage('message')?.member]
-            : input instanceof CommandInteraction
-            ? [input.options.getMember('member') as GuildMember]
-            : Array.from(
-                  input.mentions.members?.values() ?? ([] as GuildMember[])
-              );
-    let embed = new Discord.MessageEmbed()
+    const messageId =
+        interaction instanceof ContextMenuInteraction
+            ? interaction.options.getMessage('message')?.id
+            : interaction.channel?.lastMessageId;
+    const memberReported =
+        interaction instanceof ContextMenuInteraction
+            ? interaction.options.getMessage('message')?.member
+            : interaction.options.getMember('member');
+    let embed = new MessageEmbed()
         .setAuthor({
             name: member.user.tag,
             iconURL: member.displayAvatarURL({ dynamic: true }),
@@ -271,78 +211,80 @@ export async function report(
                     true
                 )} ago` +
                 '\n' +
-                `**Reported from:** ${channel} [Jump to context](https://discord.com/channels/804222694488932362/${channel?.id}/${messageLink})`
+                `**Reported from:** ${channel} [Jump to context](https://com/channels/804222694488932362/${channel?.id}/${messageId})`
         )
-        .setFooter({ text: `Report ID: ${input.id}` })
+        .setFooter({ text: `Report ID: ${interaction.id}` })
         .setTimestamp(createdTimestamp);
-    memberMentions.forEach((m, i) => {
-        const mentioned = guild.members.cache.get(m?.user?.id ?? '');
-        if (mentioned)
-            embed = embed.addField(
-                `Mentioned user #${i + 1}`,
-                `**Name:** ${mentioned.user.tag} ${m}` +
-                    '\n' +
-                    `**Joined at:** ${parseMsIntoReadableText(
-                        now - (mentioned.joinedTimestamp || now),
-                        true
-                    )} ago` +
-                    '\n' +
-                    `**Account Created:** ${parseMsIntoReadableText(
-                        now - mentioned.user.createdTimestamp,
-                        true
-                    )} ago` +
-                    '\n' +
-                    `**ID:** ${mentioned.user.id}`
-            );
-    });
+
+    if (memberReported)
+        embed = embed.addField(
+            memberReported.user.tag,
+            `**Member:** ${memberReported}` +
+                '\n' +
+                `**Joined at:** ${parseMsIntoReadableText(
+                    now - (memberReported.joinedTimestamp || now),
+                    true
+                )} ago` +
+                '\n' +
+                `**Account Created:** ${parseMsIntoReadableText(
+                    now - memberReported.user.createdTimestamp,
+                    true
+                )} ago` +
+                '\n' +
+                `**ID:** ${memberReported.user.id}`
+        );
+
     const logChannel = guild.channels.cache.get('806812461302022145');
     const supportCategory = guild.channels.cache.get('804230480475848754');
-    const modRole = guild.roles.cache.get('804223928427216926');
-    const tModRole = guild.roles.cache.get('807219483311603722');
+
     const { everyone } = guild.roles;
-    if (!(supportCategory instanceof Discord.CategoryChannel)) {
-        throw new Error('unable to locate server support category');
+    if (!(supportCategory instanceof CategoryChannel)) {
+        await interaction.reply({
+            content:
+                'Unable to locate server support category, please contact an admin.',
+            ephemeral: true,
+        });
+        return;
     }
     if (logChannel?.isText()) {
         await logChannel.send({ embeds: [embed.setTitle('Member Report')] });
     }
-    const reportRoom = (await guild.channels.create(
+    const reportRoom = await guild.channels.create(
         `${member.user.username}-${member.user.discriminator}-report-room`,
         {
+            type: 'GUILD_TEXT',
             parent: supportCategory,
             reason: 'Member Report',
             permissionOverwrites: [
                 {
                     id: everyone,
-                    deny: ['VIEW_CHANNEL'],
+                    deny: new Permissions(['VIEW_CHANNEL']),
                 },
                 {
                     id: member,
-                    allow: ['VIEW_CHANNEL'],
+                    allow: new Permissions(['VIEW_CHANNEL']),
                 },
-                {
-                    id: modRole ?? '',
-                    allow: ['VIEW_CHANNEL', 'MANAGE_MESSAGES'],
-                },
-                {
-                    id: tModRole ?? '',
-                    allow: ['VIEW_CHANNEL', 'MANAGE_MESSAGES'],
-                },
+                ...moderatorRoleIds.map(id => ({
+                    id,
+                    allow: new Permissions(['VIEW_CHANNEL', 'MANAGE_MESSAGES']),
+                })),
             ],
         }
-    )) as TextBasedChannel;
+    );
     const initMessage = await reportRoom.send({
-        content: `${member.user.toString()}, please wait patiently for our ${modRole} ${tModRole} team to response, please describe the details of the report if it was not fully addressed in the \`!report\` command`,
+        content: `${member}, please wait patiently for our ${moderatorRoleIds.join(
+            ' '
+        )} team to response, please describe the details of the report if it was not fully addressed in the \`/report\` command`,
         embeds: [embed],
     });
     await initMessage.pin();
-    if (!(input instanceof Message)) {
-        reply(
-            input,
-            `A report channel has been created, please wait patiently for our ${modRole} ${tModRole} team to response, please describe the details of the report if it was not fully addressed in the \`!report\` command`,
-            true
-        );
-    }
+
+    await interaction.reply({
+        content: `A report channel has been created, please wait patiently for our ${moderatorRoleIds.join(
+            ' '
+        )} team to response, please describe the details of the report if it was not fully addressed in the \`/report\` command`,
+        ephemeral: true,
+    });
 }
 
 export const commandData: ApplicationCommandData[] = [
