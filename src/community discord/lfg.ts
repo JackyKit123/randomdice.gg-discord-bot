298796fc2b0e5dbe336d60e86930ef5cf6aa5e6b
JackyKit <jackykit19970402@gmail.com>
2022-03-08 18:03:13 -0800

    refractor lfg

--- src/community discord/lfg.ts
+++ src/community discord/lfg.ts
@@ -1,23 +1,24 @@
 import channelIds from 'config/channelIds';
 import roleIds, { tier2RoleIds } from 'config/roleId';
-import Discord, {
+import {
     ApplicationCommandData,
     CommandInteraction,
+    MessageEmbed,
 } from 'discord.js';
 import cooldown from 'util/cooldown';
 import { reply } from 'util/typesafeReply';
 import checkPermission from './util/checkPermissions';
 
-export default async function lfg(input: CommandInteraction): Promise<void> {
-    if (!input.inCachedGuild()) return;
-    const { member, channel, commandName } = input;
+export default async function lfg(command: CommandInteraction): Promise<void> {
+    if (!command.inCachedGuild()) return;
+    const { member, channel, commandName } = command;
 
-    const msg = input.options.getString('message');
+    const msg = command.options.getString('message');
 
     if (!channel) return;
 
     if (
-        await cooldown(input, commandName, {
+        await cooldown(command, commandName, {
             default: 600 * 1000,
             donator: 600 * 1000,
         })
@@ -25,14 +26,14 @@ export default async function lfg(input: CommandInteraction): Promise<void> {
         return;
     }
 
-    if (!(await checkPermission(input, ...tier2RoleIds))) return;
+    if (!(await checkPermission(command, ...tier2RoleIds))) return;
 
     if (
         channel.id !== channelIds['jackykit-playground-v3'] &&
         channel.id !== channelIds['look-for-games']
     ) {
         await reply(
-            input,
+            command,
             `You can only use this command in <#${channelIds['look-for-games']}>.`
         );
         return;
@@ -40,13 +41,13 @@ export default async function lfg(input: CommandInteraction): Promise<void> {
 
     if (msg && msg.length > 1024) {
         await reply(
-            input,
+            command,
             'Your message cannot be longer than 1024 characters.'
         );
         return;
     }
 
-    let embed = new Discord.MessageEmbed()
+    let embed = new MessageEmbed()
         .setAuthor({
             name: member.user.tag,
             iconURL: member.displayAvatarURL({ dynamic: true }),
@@ -61,14 +62,14 @@ export default async function lfg(input: CommandInteraction): Promise<void> {
         embed = embed.addField('Message', msg);
     }
 
-    await input.deferReply();
+    await command.deferReply();
 
     await channel.send({
         content: `<@&${roleIds['Looking for Games Ping']}>`,
         embeds: [embed],
     });
 
-    await input.deleteReply();
+    await command.deleteReply();
 }
 
 export const commandData: ApplicationCommandData = {
