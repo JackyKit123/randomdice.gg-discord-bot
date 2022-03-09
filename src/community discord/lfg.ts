92695d90b8f9a6398b9b0d944b4198563dfdbfa3
JackyKit <jackykit19970402@gmail.com>
2022-03-08 16:36:00 -0800

    lfg command is now slash command only

--- src/community discord/lfg.ts
+++ src/community discord/lfg.ts
@@ -2,29 +2,21 @@ import roleIds, { tier2RoleIds } from 'config/roleId';
 import Discord, {
     ApplicationCommandData,
     CommandInteraction,
-    Message,
 } from 'discord.js';
 import cooldown from 'util/cooldown';
 import { reply } from 'util/typesafeReply';
 import checkPermission from './util/checkPermissions';
 
-export default async function lfg(
-    input: Message | CommandInteraction
-): Promise<void> {
-    const { guild, channel } = input;
+export default async function lfg(input: CommandInteraction): Promise<void> {
+    if (!input.inCachedGuild()) return;
+    const { member, channel, commandName } = input;
 
-    const member = guild?.members.cache.get(input.member?.user.id ?? '');
-    const msg =
-        input instanceof Message
-            ? input.content.replace(/!lfg ?/i, '')
-            : input.options.getString('message');
+    const msg = input.options.getString('message');
 
-    if (!member || !guild || !channel) {
-        return;
-    }
+    if (!channel) return;
 
     if (
-        await cooldown(input, '!lfg', {
+        await cooldown(input, commandName, {
             default: 600 * 1000,
             donator: 600 * 1000,
         })
@@ -68,18 +60,14 @@ export default async function lfg(
         embed = embed.addField('Message', msg);
     }
 
-    if (input instanceof Message) {
-        await input.delete();
-    } else {
-        await input.deferReply();
-    }
+    await input.deferReply();
+
     await channel.send({
         content: `<@&${roleIds['Looking for Games Ping']}>`,
         embeds: [embed],
     });
-    if (input instanceof CommandInteraction) {
-        await input.deleteReply();
-    }
+
+    await input.deleteReply();
 }
 
 export const commandData: ApplicationCommandData = {
