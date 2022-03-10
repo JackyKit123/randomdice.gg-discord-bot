2db9c85685973702682406472c4bebc0d2c069a6
JackyKit <jackykit19970402@gmail.com>
2022-03-09 18:22:31 -0800

    update spy.ts

--- src/community discord/spy.ts
+++ src/community discord/spy.ts
@@ -1,7 +1,6 @@
 import * as Discord from 'discord.js';
-import logMessage from 'dev-commands/logMessage';
 import channelIds from 'config/channelIds';
-import roleIds, { moderatorRoleIds } from 'config/roleId';
+import { moderatorRoleIds } from 'config/roleId';
 
 const bannedCache: string[] = [];
 
@@ -37,107 +36,93 @@ async function fetchMember(
 }
 
 export default async function spy(message: Discord.Message): Promise<void> {
-    try {
-        const { guild, member, content, client, author, channel, attachments } =
-            message;
-        if (
-            !guild ||
-            !member ||
-            guild.id !== '818961659086766111' ||
-            !channel.isText()
-        )
-            return;
+    const { guild, member, content, client, author, channel, attachments } =
+        message;
+    if (
+        !guild ||
+        !member ||
+        guild.id !== '818961659086766111' ||
+        !channel.isText()
+    )
+        return;
 
-        const communityDiscord = client.guilds.cache.get(
-            process.env.COMMUNITY_SERVER_ID ?? ''
+    const communityDiscord = client.guilds.cache.get(
+        process.env.COMMUNITY_SERVER_ID ?? ''
+    );
+    const spyLog = communityDiscord?.channels.cache.get(
+        channelIds['hack-discord-spy-log']
+    );
+    if (!communityDiscord || !spyLog) return;
+    const isBanned = await fetchIsBanned(communityDiscord, author);
+    const isCommunityDiscordMember = isBanned
+        ? false
+        : await fetchMember(communityDiscord, author);
+    if (!spyLog?.isText()) return;
+    const sensitiveWords =
+        /\b(hack\w*)|(buy\w*)|(sell\w*)|(boost\w*)|(account\w*)|(price\w*)|(carry\w*)|(carried)|(c(?:lass)? ?15)|(\$)\b/gi;
+    const triggered = Array.from(content.matchAll(sensitiveWords));
+    const [sliced1, sliced2] = [content.slice(0, 1024), content.slice(1024)];
+    let embed = new Discord.MessageEmbed()
+        .setAuthor({
+            name: author.tag,
+            iconURL: author.displayAvatarURL({ dynamic: true }),
+        })
+        .setTitle('Hack Discord Spied Message')
+        .addField('User', `${author}\nID: ${author.id}`)
+        .addField('User has been banned', isBanned ? '✔️' : '❌');
+    if (!isBanned) {
+        embed = embed.addField(
+            'User is member in this discord',
+            isCommunityDiscordMember ? '✔️' : '❌'
         );
-        const spyLog = communityDiscord?.channels.cache.get(
-            channelIds['hack-discord-spy-log']
-        );
-        if (!communityDiscord || !spyLog) return;
-        const isBanned = await fetchIsBanned(communityDiscord, author);
-        const isCommunityDiscordMember = isBanned
-            ? false
-            : await fetchMember(communityDiscord, author);
-        if (!spyLog?.isText()) return;
-        const sensitiveWords =
-            /\b(hack\w*)|(buy\w*)|(sell\w*)|(boost\w*)|(account\w*)|(price\w*)|(carry\w*)|(carried)|(c(?:lass)? ?15)|(\$)\b/gi;
-        const triggered = Array.from(content.matchAll(sensitiveWords));
-        const [sliced1, sliced2] = [
-            content.slice(0, 1024),
-            content.slice(1024),
-        ];
-        let embed = new Discord.MessageEmbed()
-            .setAuthor({
-                name: author.tag,
-                iconURL: author.displayAvatarURL({ dynamic: true }),
-            })
-            .setTitle('Hack Discord Spied Message')
-            .addField('User', `${author}\nID: ${author.id}`)
-            .addField('User has been banned', isBanned ? '✔️' : '❌');
-        if (!isBanned) {
-            embed = embed.addField(
-                'User is member in this discord',
-                isCommunityDiscordMember ? '✔️' : '❌'
-            );
-        }
-        embed = embed
-            .addField(
-                'In Channel',
-                `#${(channel as Discord.GuildChannel).name}`
-            )
-            .setFooter({
-                text: guild.name,
-                iconURL: guild.iconURL({ dynamic: true }) ?? undefined,
-            })
-            .setTimestamp();
-        if (!isBanned) {
-            if (triggered.length) {
-                if (isCommunityDiscordMember) {
-                    embed = embed.setColor('#ff0000');
-                } else {
-                    embed = embed.setColor('#ffff00');
-                }
-            } else if (isCommunityDiscordMember) {
-                embed = embed.setColor('#ffff00');
+    }
+    embed = embed
+        .addField('In Channel', `#${(channel as Discord.GuildChannel).name}`)
+        .setFooter({
+            text: guild.name,
+            iconURL: guild.iconURL({ dynamic: true }) ?? undefined,
+        })
+        .setTimestamp();
+    if (!isBanned) {
+        if (triggered.length) {
+            if (isCommunityDiscordMember) {
+                embed = embed.setColor('#ff0000');
             } else {
-                embed = embed.setColor('#00ff00');
+                embed = embed.setColor('#ffff00');
             }
-        }
-        if (sliced1) {
-            embed = embed.addField('Content', sliced1);
-            if (sliced2) {
-                embed = embed.addField('‎', sliced2);
-            }
-        }
-        if (attachments.size) {
-            embed = embed.addField(
-                `Attachment${attachments.size > 1 ? 's' : ''}`,
-                attachments.map(attachment => attachment.url).join('\n')
-            );
-        }
-        const sentMessage = await spyLog.send({
-            content: triggered.length
-                ? `${
-                      isBanned
-                          ? ''
-                          : moderatorRoleIds.map(id => `<@&${id}>`).join(' ')
-                  } Sensitive keyword${
-                      Array.from(triggered).length > 1 ? 's' : ''
-                  } triggered: ${Array.from(triggered)
-                      .map(match => `**${match[0]}**`)
-                      .join(' ')}`
-                : undefined,
-            embeds: [embed],
-        });
-        if (!isBanned) await sentMessage.react('868148038311489578');
-    } catch (err) {
-        try {
-            await logMessage(message.client, (err as Error).stack);
-        } catch (e) {
-            // no action
+        } else if (isCommunityDiscordMember) {
+            embed = embed.setColor('#ffff00');
+        } else {
+            embed = embed.setColor('#00ff00');
         }
     }
+    if (sliced1) {
+        embed = embed.addField('Content', sliced1);
+        if (sliced2) {
+            embed = embed.addField('‎', sliced2);
+        }
+    }
+    if (attachments.size) {
+        embed = embed.addField(
+            `Attachment${attachments.size > 1 ? 's' : ''}`,
+            attachments.map(attachment => attachment.url).join('\n')
+        );
+    }
+    const sentMessage = await spyLog.send({
+        content: triggered.length
+            ? `${
+                  isBanned
+                      ? ''
+                      : moderatorRoleIds.map(id => `<@&${id}>`).join(' ')
+              } Sensitive keyword${
+                  Array.from(triggered).length > 1 ? 's' : ''
+              } triggered: ${Array.from(triggered)
+                  .map(match => `**${match[0]}**`)
+                  .join(' ')}`
+            : undefined,
+        embeds: [embed],
+    });
+    if (!isBanned) await sentMessage.react('868148038311489578');
 }
 
 async function cleanUpMessage(
