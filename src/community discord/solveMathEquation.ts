12d19b45f99987362f1cc34d9b14f8fff74144df
JackyKit <jackykit19970402@gmail.com>
2022-03-17 16:08:19 -0700

    escapring : in solve math equation

--- src/community discord/solveMathEquation.ts
+++ src/community discord/solveMathEquation.ts
@@ -5,7 +5,7 @@ export default async function solveMathEquation(
     message: Discord.Message
 ): Promise<void> {
     const { content, channel } = message;
-    if (!content || /^(".+"|'.+'|[\d\w]+|#.*)$/.test(content)) return;
+    if (!content || /^(".+"|'.+'|[\d\w]+|#.|\d+:|:\d+*)$/.test(content)) return;
     try {
         const evaluated = math.evaluate(content);
         const formatted = math.format(evaluated, 14);
