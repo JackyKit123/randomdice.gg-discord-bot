8a82314e5858364f279a18769e434e9c34d93eb6
JackyKit <jackykit19970402@gmail.com>
2022-03-23 02:02:52 -0700

    refractor code to use reply

--- src/community discord/oneMinute.ts
+++ src/community discord/oneMinute.ts
@@ -1,21 +1,16 @@
-import Discord from 'discord.js';
+import { Message } from 'discord.js';
+import wait from 'util/wait';
 
-export default async function oneMinute(
-    message: Discord.Message
-): Promise<void> {
-    const { content, author, channel } = message;
+export default async function oneMinute(message: Message): Promise<void> {
+    const { content, author } = message;
 
     if (/\b(a|one|1) ?min(ute)?\b/i.test(content)) {
-        setTimeout(
-            () => channel.send(`${author}, it's been one minute.`),
-            1000 * 60
-        );
+        await wait(1000 * 60);
+        await message.reply(`${author}, it's been one minute.`);
     }
 
     if (/\b(a|one|1) ?sec(ond)?\b/i.test(content)) {
-        setTimeout(
-            () => channel.send(`${author}, it's been one second.`),
-            1000
-        );
+        await wait(1000);
+        await message.reply(`${author}, it's been one second.`);
     }
 }
