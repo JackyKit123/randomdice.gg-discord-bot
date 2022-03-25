d95157ca5432ae356d78f64b02f1cf5001957020
JackyKit <jackykit19970402@gmail.com>
2022-03-25 16:07:07 -0700

    added back free clowning

--- src/community discord/currency/fun commands/clown.ts
+++ src/community discord/currency/fun commands/clown.ts
@@ -1,13 +1,11 @@
 import roleIds from 'config/roleId';
 import { ApplicationCommandData, Client, CommandInteraction } from 'discord.js';
-import { promisify } from 'util';
 import cooldown from 'util/cooldown';
 import { clown as clownEmoji } from 'config/emojiId';
 import { getCommunityDiscord } from 'config/guild';
 import { isJackykit } from 'config/users';
+import wait from 'util/wait';
 import commandCost from './commandCost';
-
-const wait = promisify(setTimeout);
 
 export default async function clown(
     interaction: CommandInteraction
@@ -20,41 +18,54 @@ export default async function clown(
         (await cooldown(interaction, {
             default: 60 * 1000 * 5,
             donator: 60 * 1000 * 1,
-        })) ||
+        }))
+    ) {
+        return;
+    }
+    if (
         !(await commandCost(
             interaction,
             Math.round(Math.random() * 3500 - 1500)
         ))
     ) {
-        return;
+        await interaction.followUp(
+            "Usually that's the case, but today I am gonna allow you to use it.<a:clowndance:845532985787940894>"
+        );
     }
     let target = options.getMember('member', true);
 
     if (target?.id === member.id && member.roles.cache.has(roleIds['🤡'])) {
-        await interaction.reply('Slow Down. You are already a clown, jeez.');
+        await (interaction.replied
+            ? interaction.followUp('Slow Down. You are already a clown, jeez.')
+            : interaction.reply('Slow Down. You are already a clown, jeez.'));
         return;
     }
-    await interaction.reply(
-        'https://media.tenor.com/images/87126cc81f03e22938d296cc5a60b2d2/tenor.gif'
-    );
+
+    const response = await (interaction.replied
+        ? interaction.followUp(
+              'https://media.tenor.com/images/87126cc81f03e22938d296cc5a60b2d2/tenor.gif'
+          )
+        : interaction.reply({
+              content:
+                  'https://media.tenor.com/images/87126cc81f03e22938d296cc5a60b2d2/tenor.gif',
+              fetchReply: true,
+          }));
     await wait(4700);
 
     let clownedABot = false;
     if (target.id === member.id) {
-        await interaction.editReply(
+        await response.edit(
             `${member}, you have a weird interest, but yes you can be a clown yourself, now entertain us.`
         );
     } else if (isJackykit(member)) {
-        await interaction.editReply(
-            `${target} got clowned by ${member}.${clownEmoji}`
-        );
+        await response.edit(`${target} got clowned by ${member}.${clownEmoji}`);
     } else if (target.roles.cache.has(roleIds['🤡'])) {
-        await interaction.editReply(
+        await response.edit(
             `${target} has already been clowned. Why are you so desperate? I guess you are the real clown then.`
         );
         target = member;
     } else if (target.user.bot) {
-        await interaction.editReply(
+        await response.edit(
             `What's wrong in your mind to clown a bot? Good Try tho, you clown.`
         );
         clownedABot = true;
@@ -63,47 +74,38 @@ export default async function clown(
         isJackykit(target) ||
         (target.id !== member.id && Math.random() < 0.6)
     ) {
-        await interaction.editReply(
+        await response.edit(
             `${member} is trying clown ${target}. **BUT IT BACKFIRED, ${member} is now a clown LOL!!!**`
         );
         target = member;
     } else {
-        await interaction.editReply(
-            `${target} got clowned by ${member}.${clownEmoji}`
-        );
+        await response.edit(`${target} got clowned by ${member}.${clownEmoji}`);
     }
     const originalName = target.displayName;
     const howClown =
         clownedABot || isJackykit(member) ? 10 : Math.ceil(Math.random() * 10);
-    try {
-        await target.roles.add(roleIds['🤡']);
-        await target.setNickname('🤡'.repeat(howClown));
-    } catch (err) {
-        // suppress error
-    } finally {
-        await channel.send({
-            content: `${target} ${
-                clownedABot
-                    ? 'tried to clown a bot. 100%'
-                    : `is a ${howClown * 10}%`
-            } clown!${clownEmoji}`,
-            allowedMentions: {
-                users: [],
-                roles: [],
-                parse: [],
-            },
-        });
-        await wait(1000 * 60 * 5);
-        try {
-            if (target.roles.cache.has(roleIds['🤡'])) {
-                await target.roles.remove(roleIds['🤡']);
-            }
-            await wait(5000);
-            await target.setNickname(originalName);
-        } catch (err) {
-            // suppress error
-        }
-    }
+
+    await target.roles.add(roleIds['🤡']);
+    if (target.manageable) await target.setNickname('🤡'.repeat(howClown));
+
+    await interaction.followUp({
+        content: `${target} ${
+            clownedABot
+                ? 'tried to clown a bot. 100%'
+                : `is a ${howClown * 10}%`
+        } clown!${clownEmoji}`,
+        allowedMentions: {
+            users: [],
+            roles: [],
+            parse: [],
+        },
+    });
+    await wait(1000 * 60 * 5);
+
+    await target.roles.remove(roleIds['🤡']);
+
+    await wait(5000);
+    if (target.manageable) await target.setNickname(originalName);
 }
 
 export async function fixClownNicknamesOnReboot(
